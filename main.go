3c8113beaac84f4f6848ed485fca07dd2b7b259f
Ilya Lebedev <cepctapk@gmail.com>
2024-04-28 17:36:21 +0300

    add go mod go sum

--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net/http"
+	"os"
+)
+
+const (
+	webDir      = "./web"
+	defaultPort = "7540"
+)
+
+func main() {
+
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = ":" + defaultPort
+	}
+
+	http.Handle("/", http.FileServer(http.Dir(webDir)))
+
+	err := http.ListenAndServe(port, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+}
